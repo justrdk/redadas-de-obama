c05a25c4b7a18b7d85f8465c329b9835ceb26877
Celso Mireles <celso@digitalstrategy.tips>
2016-05-18 16:46:02 -0700

    Add toggle component for alerts section

--- imports/ui/components/AlertOptions.jsx
+++ imports/ui/components/AlertOptions.jsx
@@ -4,13 +4,33 @@ import ReactDOM from 'react-dom';
 import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 import { Meteor } from 'meteor/meteor';
+import Toggle from 'react-toggle';
 
 export default class AlertOptions extends TrackerReact(Component) {
+
+	constructor(props) {
+	    super(props);
+
+	    this.state = {
+				alertStatus: false
+	    };
+	}
+
+	handleAlertChange() {
+		this.state.alertStatus = !this.state.alertStatus;
+		console.log(this.state.alertStatus);
+	}
 
 	render() {
 		return (
 			<div>
-				Toggle
+				<label htmlFor="cheese-status">Recibir Alertas</label>
+
+				<Toggle
+				  id="alert-status"
+				  defaultChecked={this.state.alertStatus}
+				  onChange={this.handleAlertChange} />
+
 			</div>
 		)
 	}
