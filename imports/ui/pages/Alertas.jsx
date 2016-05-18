ac8127c342c3822cbd8028260588d1c2790c58dc
Celso Mireles <celso@digitalstrategy.tips>
2016-05-18 16:01:03 -0700

    Add login button to Alerts page

--- imports/ui/pages/Alertas.jsx
+++ imports/ui/pages/Alertas.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import {mount} from 'react-mounter';
+import ReactDOM from 'react-dom';
+import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
-export default class Alertas extends Component {
+export default class Alertas extends TrackerReact(Component) {
+
+	componentDidMount() {
+		ReactDOM.render(<LogInButtonsDialog />, document.getElementById('login'));
+	}
+
+	componentWillUnmount() {
+
+  }
+
 	render() {
 		return (
 			<div>
 				<h2>
 					Alertas
 				</h2>
+
+				<div id="login" className="alt-accounts-log-in-buttons"></div>
 
 			</div>
 		)
