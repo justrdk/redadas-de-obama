04e002d5f54bd4e8082b1261b49d9771325937a5
Celso Mireles <celso@digitalstrategy.tips>
2016-05-18 17:04:44 -0700

    Change report form field language

--- imports/ui/components/ReportForm.jsx
+++ imports/ui/components/ReportForm.jsx
@@ -57,8 +57,8 @@ export default class ReportForm extends TrackerReact(Component) {
 		return (
 			<div>
 				<form onSubmit={this.insertRaid}>
-					<input id="txtDescription" type="text" placeholder="Describe the raid" />
-					<input id="txtAddress" type="text" placeholder="Zip Code" />
+					<input id="txtDescription" type="text" placeholder="Describe la redada" />
+					<input id="txtAddress" type="text" placeholder="Direccion o Codigo Postal" />
 
 					<input type="submit" className="report-submit" />
 				</form>
