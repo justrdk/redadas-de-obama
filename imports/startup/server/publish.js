6505bf75710db07bf920864579a37cee990bffb6
Celso Mireles <celso@digitalstrategy.tips>
2016-05-20 16:46:35 -0700

    Create an AlertStatus component
    need to find a way to reactively display. but need to be able to pull
    info from user profile first.

--- imports/startup/server/publish.js
+++ imports/startup/server/publish.js
@@ -4,4 +4,11 @@ if(Meteor.isServer){
 	Meteor.publish('allRaids', function() {
 		return Raids.find();
 	});
+
+	Meteor.publish(null, function() {
+  return Meteor.users.find({_id: this.userId}, {fields: {
+		'profile.getsAlerts': 1
+	}});
+});
+
 }
